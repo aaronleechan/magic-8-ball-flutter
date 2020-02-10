48badeb23dbab213085868ca7186dc18ff0f7f20
aaronleechan <aaron171091@gmail.com>
2020-02-09 17:21:37 -0800

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,46 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blueAccent[400],
+          appBar: AppBar(
+            title: Center(child: Text("Magic Ball")),
+          ),
+          body: MagicPage(),
+        ),
       ),
     );
+
+class MagicPage extends StatefulWidget {
+  @override
+  _MagicPageState createState() => _MagicPageState();
+}
+
+class _MagicPageState extends State<MagicPage> {
+  int numberofVall = 1;
+
+  void pressButton() {
+    Random random = new Random();
+    setState(() {
+      numberofVall = random.nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: pressButton,
+            child: Image.asset(
+              'images/ball${numberofVall}.png',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
